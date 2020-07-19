1f2aee4f13f95f95fb822ecccc2a6956fa8fc175
Kaciras <Kaciras@outlook.com>
2020-07-19 16:36:37 +0800

    (WIP)

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import style from "./home.module.scss";
+
+function ResumeCard({ title, href, banner }) {
+	return (
+		<Link href={href}>
+			<a className={style.card}>
+				<img className={style.banner} alt="banner" src={banner} />
+				<h2 className={style.title}>{title}</h2>
+			</a>
+		</Link>
+	);
+}
 
 export default function Home() {
 	return (
-		<div className="container">
-			<Link href="/web.html">
-				<a>Web前端开发工程师</a>
-			</Link>
+		<div className={style.container}>
+			<ResumeCard
+				title="Web前端开发工程师"
+				href="/web"
+				banner={require("../assets/website-design.svg")}
+			/>
 		</div>
 	);
 }
