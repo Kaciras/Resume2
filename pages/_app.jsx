616c271f7f7228d7a629dc46c5b59dd797d431cd
Kaciras <Kaciras@outlook.com>
2020-10-06 16:52:42 +0800

    rename react component files with suffix .jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "@/css/global.scss";
 import "@/css/mobile.scss";
 
-// This default export is required in a new `pages/_app.js` file.
+// This default export is required in a new `pages/_app.jsx` file.
 export default function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
--- pages/backend/index.jsx
+++ pages/backend/index.jsx
@@ -12,7 +12,7 @@ export default function BackendResume() {
 				<base target="_blank" />
 			</Head>
 			<ResumeHeader title="JAVA工程师" />
-			<Section title="TODO" subtitle="编织有艺术感的代码"></Section>
+			<Section title="TODO" subtitle="编织有艺术感的代码" />
 			<Footer />
 		</>
 	);
