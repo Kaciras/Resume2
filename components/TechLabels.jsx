15c379778a977eff980c82d156e25696c2c90a15
Kaciras <Kaciras@outlook.com>
2020-10-18 14:33:16 +0800

    TechLabels: avoid word wrapping

--- components/TechLabels.jsx
+++ components/TechLabels.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import clsx from "clsx";
 import styles from "./TechLabels.module.scss";
 
+/**
+ * 一组彩色的标签，表示项目中用到的各种技术，不同的种类有不同的颜色。
+ * 请保证技术名不要超出单行宽度，过长的名字请使用简称。
+ *
+ * @param stack 技术栈
+ */
 export default function TechLabels({ stack }) {
 	const labels = [];
 
 	function render(type) {
+		const clazz = clsx(styles.label, styles[type]);
 		(stack[type] || []).forEach(name =>
 			labels.push(
-				<span key={name} className={clsx(styles.label, styles[type])}>
+				<span key={name} className={clazz}>
 					{name}
 				</span>
 			)
