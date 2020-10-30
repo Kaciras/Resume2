bf342f0e986c389d47885de9340a5c5558997084
Kaciras <Kaciras@outlook.com>
2020-10-30 10:57:08 +0800

    webpack: fix .mjs file resolving

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ function customWebpack(config) {
 		test: /\.(?:md|encrypt)$/,
 		use: "raw-loader",
 	});
+
+	// https://github.com/vercel/next.js/issues/17806
+	config.module.rules.push({
+		test: /\.mjs$/,
+		type: "javascript/auto",
+	});
+
 	config.resolve.alias["@"] = __dirname;
 	return config;
 }
