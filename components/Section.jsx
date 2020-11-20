30b83afc1bd11ad8969b01c20484a27f7054785d
Kaciras <Kaciras@protonmail.com>
2020-11-20 17:09:55 +0800

    optimize style

--- components/Section.jsx
+++ components/Section.jsx
@@ -16,10 +16,8 @@ export default function Section(props) {
 
 	return (
 		<section className={styles[type]}>
-			<div className={styles.content}>
-				{header}
-				{children}
-			</div>
+			{header}
+			{children}
 		</section>
 	);
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import "@/css/global.scss";
-import "@/css/mobile.scss";
+import "@/css/index.scss";
 
 // This default export is required in a new `pages/_app.jsx` file.
 export default function MyApp({ Component, pageProps }) {
