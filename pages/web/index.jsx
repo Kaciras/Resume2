3fcbbb1b18bb9187b776f7a0cdcdf517b01c7cc5
Kaciras <Kaciras@protonmail.com>
2020-11-23 14:20:15 +0800

    refactor page

--- pages/web/index.jsx
+++ pages/web/index.jsx
@@ -12,39 +12,88 @@ import blogMarkdown from "./Blog.md";
 import icAnalyzeMarkdown from "./ICAnalyze.md";
 import skillMarkdown from "./Skill.md";
 
-const blogStack = {
-	lang: ["JAVA", "TypeScript"],
-	frontend: ["NodeJS", "Webpack", "PWA", "Vue.js", "Storybook"],
-	backend: ["SpringBoot", "Redis", "MySQL", "MyBatis"],
-	operation: ["Nginx", "Debian", "Travis CI"],
+const blogProject = {
+	name: "个人网站",
+	links: [
+		{
+			href: "https://blog.kaciras.com",
+			text: "blog.kaciras.com",
+		},
+		{
+			href: "https://github.com/kaciras-blog",
+			text: "GitHub",
+		},
+	],
+	techStack: {
+		lang: ["JAVA", "TypeScript"],
+		frontend: ["NodeJS", "Webpack", "PWA", "Vue.js", "Storybook"],
+		backend: ["SpringBoot", "Redis", "MySQL", "MyBatis"],
+		operation: ["Nginx", "Debian", "Travis CI"],
+	},
+	banner: screenshot,
+	content: blogMarkdown,
 };
 
-const icaStack = {
-	lang: ["TypeScript", "C++", "WebAssembly"],
-	frontend: ["React", "Webpack", "WebWorker", "Echarts"],
-	operation: ["GitHub Actions"],
+const ICAnalyze = {
+	name: "图片编码分析工具",
+	links: [
+		{
+			href: "https://ic-analyze.kaciras.com",
+			text: "ic-analyze.kaciras.com",
+		},
+		{
+			href: "https://github.com/Kaciras/ICAnalyze",
+			text: "GitHub",
+		},
+	],
+	techStack: {
+		lang: ["TypeScript", "C++", "WebAssembly"],
+		frontend: ["React", "Webpack", "WebWorker", "Echarts"],
+		operation: ["GitHub Actions"],
+	},
+	banner: screenshot,
+	content: icAnalyzeMarkdown,
 };
+
+// 因为在链接内用伪元素能够点击，所以还是单独用元素做分隔
+function Delimiter() {
+	return <span className={style.delimiter} role="separator">|</span>;
+}
 
 function ProjectIntro(props) {
 	const { name, links, techStack, banner, content } = props;
+
+	const linkNodes = links.map(link => (
+		<ExternalLink
+			key={link.href}
+			href={link.href}
+		>
+			{link.text}
+		</ExternalLink>
+	));
+
+	// 可惜数组的 join 不能使用函数，还得自己循环做。
+	const items = [];
+	for (let i = 0; i < linkNodes.length - 1; i++) {
+		items.push(linkNodes[i]);
+		items.push(<Delimiter key={i}/>);
+	}
+	items.push(linkNodes[linkNodes.length - 1]);
+
 	return (
 		<section className={style.project}>
 			<header className={style.projectHeader}>
-				<h3>个人网站</h3>
-				<div>
-					<ExternalLink href="https://blog.kaciras.com">blog.kaciras.com</ExternalLink>
-					&nbsp;|&nbsp;
-					<ExternalLink href="https://github.com/kaciras-blog">GitHub</ExternalLink>
-				</div>
+				<h3>{name}</h3>
+				<div>{items}</div>
 			</header>
-			<TechLabels stack={blogStack}/>
+			<TechLabels stack={techStack}/>
 			<img
-				src={screenshot}
-				alt="screenshot"
-				className={style.screenshot}
+				src={banner}
+				alt="banner"
+				className={style.banner}
 			/>
 			<div className={style.markdown}>
-				<ReactMarkdown>{blogMarkdown}</ReactMarkdown>
+				<ReactMarkdown>{content}</ReactMarkdown>
 			</div>
 		</section>
 	);
@@ -58,53 +107,21 @@ export default function FrontendResume() {
 				<base target="_blank"/>
 			</Head>
 
-			<PersonalDetails title="Web前端开发工程师"/>
+			<PersonalDetails title="Web 前端开发工程师"/>
 
-			<Section title="项目展示" subtitle="编织有艺术感的代码" type="projects">
-
-				<section className={style.project}>
-					<header className={style.projectHeader}>
-						<h3>个人网站</h3>
-						<div>
-							<ExternalLink href="https://blog.kaciras.com">blog.kaciras.com</ExternalLink>
-							&nbsp;|&nbsp;
-							<ExternalLink href="https://github.com/kaciras-blog">GitHub</ExternalLink>
-						</div>
-					</header>
-					<TechLabels stack={blogStack}/>
-					<img
-						src={screenshot}
-						alt="screenshot"
-						className={style.screenshot}
-					/>
-					<div className={style.markdown}>
-						<ReactMarkdown>{blogMarkdown}</ReactMarkdown>
-					</div>
-				</section>
-
-				<section className={style.project}>
-					<header className={style.projectHeader}>
-						<h3 className={style.projectName}>在线图片编码分析工具</h3>
-						<div>
-							<ExternalLink href="https://ic-analyze.kaciras.com">ic-analyze.kaciras.com</ExternalLink>
-							&nbsp;|&nbsp;
-							<ExternalLink href="https://github.com/Kaciras/ICAnalyze">GitHub</ExternalLink>
-						</div>
-					</header>
-
-					<TechLabels stack={icaStack}/>
-					<img
-						src={screenshot}
-						alt="screenshot"
-						className={style.screenshot}
-					/>
-					<div className={style.markdown}>
-						<ReactMarkdown>{icAnalyzeMarkdown}</ReactMarkdown>
-					</div>
-				</section>
+			<Section
+				title="项目展示"
+				subtitle="编织有艺术感的代码"
+				type="projects"
+			>
+				{ProjectIntro(blogProject)}
+				{ProjectIntro(ICAnalyze)}
 			</Section>
 
-			<Section title="拥有全栈技能树" subtitle="广泛的知识不惧任何挑战">
+			<Section
+				title="拥有全栈技能树"
+				subtitle="广泛的知识不惧任何挑战"
+			>
 				<div className={style.markdown}>
 					<ReactMarkdown>{skillMarkdown}</ReactMarkdown>
 				</div>
