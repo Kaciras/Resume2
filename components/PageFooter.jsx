9d7bca6b32b9763bbcdae5f79bf15e868eefce44
Kaciras <Kaciras@protonmail.com>
2020-11-23 14:55:19 +0800

    PageFooter: wrap QRCode with a link

--- components/PageFooter.jsx
+++ components/PageFooter.jsx
@@ -1,21 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import QRCode from "qrcode";
+import ExternalLink from "./ExternalLink";
 import styles from "./PageFooter.module.scss";
-import ExternalLink from "@/components/ExternalLink";
-
-function drawQRCode(canvas) {
-	const options = {
-		margin: 1,
-		color: { light: "#fafafaff" },
-	};
-	QRCode.toDataURL(canvas, location.href, options, (err, url) => {
-		canvas.onclick = () => window.open(url, "_blank");
-	});
-}
 
 export default function PageFooter() {
+	const [qrCodeUrl, setQRCodeUrl] = useState(null);
 	const qrCodeRef = useRef(null);
-	useEffect(() => drawQRCode(qrCodeRef.current), []);
+
+	function drawQRCode() {
+		const canvas = qrCodeRef.current;
+		const options = {
+			margin: 1,
+			color: { light: "#fafafaff" },
+		};
+		QRCode.toDataURL(canvas, location.href, options).then(setQRCodeUrl);
+	}
+
+	useEffect(drawQRCode, []);
 
 	return (
 		<footer className={styles.container}>
@@ -25,7 +26,9 @@ export default function PageFooter() {
 					<ExternalLink href="https://github.com/Kaciras/Resume2"/>
 				</p>
 			</div>
-			<canvas title="本页的二维码" className={styles.qrcode} ref={qrCodeRef}/>
+			<a href={qrCodeUrl} className={styles.qrcode}>
+				<canvas ref={qrCodeRef} title="本页的二维码"/>
+			</a>
 		</footer>
 	);
 }
