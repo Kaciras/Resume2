f28734d578ce25263f0cd16ef4aa86f521eae6e7
Kaciras <Kaciras@protonmail.com>
2021-04-19 10:38:59 +0800

    (WIP) try refactor index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,55 @@
-import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-import style from "./index.module.scss";
+import PageFooter from "@/components/PageFooter";
+import styles from "./index.module.scss";
 
-function ResumeCard({ title, href, background }) {
+/*
+ * TODO: 首页正在设计中，反正目前用不上慢慢搞了。
+ *
+ * https://vincent-cotro.welovedevs.com 中规中矩，个人展示我还是希望有点创意。
+ */
+
+function ResumeCard({ title, href, image }) {
 	if (typeof location !== "undefined" && location.search) {
 		href += location.search;
 	}
+	const style = {
+		backgroundImage: `url("${image}")`,
+	};
 	return (
 		<Link href={href}>
-			<a className={style.card}>
-				<img className={style.background} alt="background" src={background} />
-				<h2 className={style.title}>{title}</h2>
+			<a className={styles.card} style={style}>
+				<div className={styles.title}>{title}</div>
 			</a>
 		</Link>
 	);
 }
 
-// TODO: 丑死了，得重新设计一下
 export default function Home() {
 	return (
-		<div className={style.container}>
+		<>
 			<Head>
 				<title>导航页 | 简历</title>
 			</Head>
-			<ResumeCard title="Web前端开发工程师" href="/web" background={require("@/assets/frontend.png")} />
-			<ResumeCard title="JAVA开发工程师" href="/backend" background={require("@/assets/java.png")} />
-		</div>
+			<section className={styles.header}>
+				<img
+					className={styles.avatar}
+					title="真实相貌以面试为准"
+					alt="avatar"
+					src="https://avatars.githubusercontent.com/u/16855468?s=460"
+				/>
+				<div className={styles.info}>
+					<h1>Kaciras</h1>
+					<h2 className={styles.sub}>Full-stack developer</h2>
+					<a href="https://blog.kaciras.com">Blog</a>
+				</div>
+			</section>
+			<section className={styles.cardList}>
+				<ResumeCard href="/web" title="前端开发" image={require("@/assets/frontend.png")}/>
+				<ResumeCard href="/web" title="JAVA 工程师" image={require("@/assets/frontend.png")}/>
+				<ResumeCard href="/web" title="JAVA 工程师" image={require("@/assets/frontend.png")}/>
+			</section>
+			<PageFooter/>
+		</>
 	);
 }
