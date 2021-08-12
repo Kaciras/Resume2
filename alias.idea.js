ccf1536d92b613cbb7e365968ff259de3e9cde6d
Kaciras <Kaciras@protonmail.com>
2021-08-12 19:26:07 +0800

    chore

--- alias.idea.js
+++ alias.idea.js
@@ -1,6 +1,6 @@
 /**
- * 本文件对构建无任何影响，仅作为 WebStorm 识别用。
- * 在 File -> Settings -> Languages & Frameworks -> JavaScript -> Webpack 选择该文件即可。
+ * 把别名记录在该文件里，让 WebStorm 能够识别。
+ * 在 File -> Settings -> Languages & Frameworks -> JavaScript -> Webpack 选择该文件。
  */
 module.exports = {
 	resolve: {
