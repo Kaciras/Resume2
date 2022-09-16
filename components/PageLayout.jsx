2260138f2f217402cd51dda73b60bb628685eabe
Kaciras <Kaciras@protonmail.com>
2022-09-16 21:46:58 +0800

    ensure title has only 1 children element

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -4,10 +4,13 @@ import Head from "next/head";
 export default function PageLayout(props) {
 	const { title, children } = props;
 
+	// 不能用 JSX 组合，因为会生成两个文本节点。
+	const fullTitle = title + " | 简历";
+
 	return (
 		<>
 			<Head>
-				<title>{title} | 简历</title>
+				<title>{fullTitle}</title>
 				<base target="_blank"/>
 			</Head>
 			{children}
