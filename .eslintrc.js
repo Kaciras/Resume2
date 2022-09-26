43537ac1e7a558526d97addf163ab93165710a4d
Kaciras <Kaciras@protonmail.com>
2022-09-26 13:23:30 +0800

    add eslint-config-next

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
 	extends: [
 		"@kaciras/core",
 		"@kaciras/react",
+		"next",
 	],
 	env: {
 		node: true,
@@ -10,6 +11,12 @@ module.exports = {
 	},
 	rules: {
 		"react/prop-types": "off",
+
+		// 自己实现了图片的加载，不用它的 Image 组件。
+		"@next/next/no-img-element": "off",
+
+		// 不识别解构，反正我也不会忘记加 alt，关了吧。
+		"jsx-a11y/alt-text": "off",
 	},
 	overrides: [{
 		files: "**/__tests__/**/*.?(m)js",
