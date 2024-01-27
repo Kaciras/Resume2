bbebc0ac872dcea4beb2792f0b50ef42a1738aa8
Kaciras <Kaciras@protonmail.com>
2024-01-27 09:31:14 +0800

    the mjs workaround is no longer needed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 function customWebpack(config) {
-	// https://github.com/vercel/next.js/issues/17806
-	config.module.rules.push({
-		test: /\.mjs$/,
-		type: "javascript/auto",
-	});
-
 	/*
 	 * image-loader 将 SVG 转换为 URL，而图标又需要支持设置属性，
  	 * 所以选择 svgr 作为加载器，并用 oneOf 避免重复处理。
