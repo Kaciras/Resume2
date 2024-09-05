8cff301193889b477c243877001421eaa43f6efe
Kaciras <Kaciras@protonmail.com>
2024-09-05 21:43:32 +0800

    less projects

--- pages/web/index.jsx
+++ pages/web/index.jsx
@@ -18,7 +18,6 @@ import reactLogo from "devicon/icons/react/react-original.svg";
 import viteLogo from "../../assets/logo/vite.svg";
 import typescriptLogo from "devicon/icons/typescript/typescript-original.svg";
 import javascriptLogo from "devicon/icons/javascript/javascript-original.svg";
-import csharpLogo from "devicon/icons/csharp/csharp-original.svg";
 import webpackLogo from "devicon/icons/webpack/webpack-original.svg";
 import nodeLogo from "devicon/icons/nodejs/nodejs-original.svg";
 import cssLogo from "devicon/icons/css3/css3-original.svg";
@@ -75,12 +74,6 @@ export default function FrontendResume() {
 					<h1 className={styles.h1}>其它项目</h1>
 					<div className={styles.cards}>
 						<ProjectCard
-							name="ts-directly"
-							description="7.5 KB（压缩后）库让 Node 直接运行 TS，自动选择已安装的编译器，支持 SWC、esbuild、sucrase、tsc。"
-							url="https://github.com/Kaciras/ts-directly"
-							icon={typescriptLogo}
-						/>
-						<ProjectCard
 							name="deasync"
 							description="能将异步代码转为同步的 NodeJS 库。"
 							url="https://github.com/Kaciras/deasync"
@@ -97,12 +90,6 @@ export default function FrontendResume() {
 							description="仅 20KB 的极简新标签页，使用 WebComponent 和 Rollup 构建。"
 							url="https://github.com/Kaciras/bookshelf"
 							icon={javascriptLogo}
-						/>
-						<ProjectCard
-							name="CodingFS"
-							description="将项目映射到虚拟分区，里面仅显示特定类型（源码、依赖、构建结果）的文件"
-							url="https://github.com/Kaciras/CodingFS"
-							icon={csharpLogo}
 						/>
 					</div>
 				</article>
