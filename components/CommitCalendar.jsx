24df1e2ee4a9117986d039384d300ddd9b744e81
Kaciras <Kaciras@protonmail.com>
2024-09-23 17:39:18 +0800

    use subgrid layout for CommitCalendar

--- components/CommitCalendar.jsx
+++ components/CommitCalendar.jsx
@@ -2,6 +2,21 @@ import clsx from "clsx";
 import data from "../lib/commits.json" with { type: "json" };
 import styles from "./CommitCalendar.module.scss";
 
+function getTooltip(commit) {
+	if (commit.level === -1) {
+		return null;
+	}
+	const date = new Date(commit.date).toISOString().split("T")[0];
+	switch (commit.count) {
+		case 0:
+			return `No contributions on ${date}`;
+		case 1:
+			return `1 contribution on ${date}`;
+		default:
+			return `${commit.count} contributions on ${date}`;
+	}
+}
+
 export default function CommitCalendar(props) {
 	const { begin, end, commits } = data;
 	const fromMonth = new Date(begin).getMonth();
@@ -9,39 +24,34 @@ export default function CommitCalendar(props) {
 
 	const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	months.push(...months.splice(0, fromMonth));
-	months.push(toMonth === fromMonth ? months[0] : "")
+	months.push(toMonth === fromMonth ? months[0] : "");
 
 	let totalCommits = 0;
 
 	const tiles = commits.map(c => {
 		totalCommits += c.count;
-		let title;
-		if (c.level !== 0) {
-			const date = new Date(c.date).toISOString().split("T")[0];
-			const x = c.count > 1 ? "contributions" : "contribution";
-			title = `${c.count} ${x} on ${date}`;
-		}
-		return <i className={styles.tile} title={title} data-level={c.level}/>;
+		return <i className={styles.tile} data-level={c.level} title={getTooltip(c)}/>;
 	});
 
 	return (
 		<div className={clsx(styles.container, props.className)}>
-			<div className={styles.header}>
-				{months.map(m => <span>{m}</span>)}
-			</div>
-			<div className={styles.weeks}>
-				<span>Mon</span>
-				<span>Wed</span>
-				<span>Fri</span>
-			</div>
+			<div style={{ gridColumn: `1/5`, gridRow: "1/2" }}></div>
+
+			{months.map(m => <span className={styles.month}>{m}</span>)}
+
+			<span className={styles.week}>Mon</span>
+			<span className={styles.week}>Wed</span>
+			<span className={styles.week}>Fri</span>
+
 			<div className={styles.tiles}>
 				{tiles}
 			</div>
-			<span className={styles.total}>
+			<div className={styles.total}>
 				{totalCommits} contributions in the last year
-			</span>
+			</div>
 			<div className={styles.legend}>
 				Less
+				<i className={styles.tile} data-level={0}/>
 				<i className={styles.tile} data-level={1}/>
 				<i className={styles.tile} data-level={2}/>
 				<i className={styles.tile} data-level={3}/>
