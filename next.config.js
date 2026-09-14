1110b890ef75d1dc494e968145bd3f02f8ec5725
Kaciras <contact@kaciras.com>
2026-09-14 15:57:42 +0800

    fix the SVG loader for Turbopack

--- next.config.js
+++ next.config.js
@@ -1,31 +1,18 @@
 import nextMDX from "@next/mdx";
 
-function customWebpack(config) {
-	/*
-	 * image-loader 将 SVG 转换为 URL，而图标又需要支持设置属性，
- 	 * 所以选择 svgr 作为加载器，并用 oneOf 避免重复处理。
-	 */
-	const loaders = [
-		{
-			test: /\.svg$/,
-			resourceQuery: /react/,
-			use: "@svgr/webpack",
-		},
-		{
-			rules: config.module.rules,
-		},
-	];
-
-	config.module.rules = [{ oneOf: loaders }];
-	return config;
-}
-
 export default nextMDX()({
-	webpack: customWebpack,
-	poweredByHeader: false,
-	eslint: {
-		ignoreDuringBuilds: true,
+	turbopack: {
+		rules: {
+			"*.svg": [
+				{
+					condition: { query: "?react" },
+					as: "*.js",
+					loaders: ["@svgr/webpack"],
+				},
+			],
+		},
 	},
+	poweredByHeader: false,
 	images: {
 		dangerouslyAllowSVG: true,
 		formats: ["image/avif", "image/webp"],
